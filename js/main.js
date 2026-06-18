02de67b4822f7f64aa9dfed78e4bc25f19fcee56
ehwel73 <ehwelrod@gmail.com>
2026-06-18 13:51:10 +0200

    1.4

--- js/main.js
+++ js/main.js
@@ -55,3 +55,17 @@ sectionHeaders.forEach(header => {
     observer.observe(header);
 });
 
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
